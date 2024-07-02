3931cc887cd124ffc9e5249d74e5f9ea3688af25
이강산 <gangsan1414@gmail.com>
2024-07-02 21:28:17 +0900

    🎉 add:restOfSeat컬럼(남은 좌석수) 추가

--- src/shows/entities/show-detail.entity.ts
+++ src/shows/entities/show-detail.entity.ts
@@ -1,12 +1,13 @@
 import {
+  BeforeInsert,
   Column,
   Entity,
   JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
-  VirtualColumn,
 } from "typeorm";
 import { Show } from "./show.entity";
+import _ from "lodash";
 
 @Entity({
   name: "show_details",
@@ -25,6 +26,15 @@ export class ShowDetail {
   @Column({ type: "int", nullable: false })
   seat: number;
 
-  // @VirtualColumn()
-  // restOfSeat: number;
+  // 남은 좌석 수
+  @Column({ type: "int", nullable: false })
+  restOfSeat: number;
+
+  // restOfSeat = seat - 예매된 좌석
+  @BeforeInsert()
+  setRestOfSeat() {
+    if (_.isNil(this.restOfSeat)) {
+      this.restOfSeat = this.seat;
+    }
+  }
 }
