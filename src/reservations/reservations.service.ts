71e83a919eef016d185a73e37251ebe4fd7819ac
이강산 <gangsan1414@gmail.com>
2024-07-03 18:13:31 +0900

    ✨ update/transaction추가, response이쁘게추가

--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "src/users/entities/user.entity";
-import { Repository } from "typeorm";
+import { Connection, Repository } from "typeorm";
 import { Reservation } from "./entities/reservation.entity";
 import { ShowDetail } from "src/shows/entities/show-detail.entity";
 import _ from "lodash";
@@ -15,6 +15,7 @@ export class ReservationsService {
     private readonly reservationsRepository: Repository<Reservation>,
     @InjectRepository(ShowDetail)
     private readonly showDetailsRepository: Repository<ShowDetail>,
+    private connection: Connection,
   ) {}
 
   async createReservation(showDetailId: number, userId: number) {
@@ -40,32 +41,86 @@ export class ReservationsService {
       throw new NotFoundException("사용자의 금액이 부족합니다.");
     }
 
-    // ---- 트랜잭션 걸어야 되는 부분 ----
-    const reservation = this.reservationsRepository.create({ showDetail, user });
-    // 예매 생성
-    await this.reservationsRepository.save(reservation);
+    // IsolationLevel = "READ UNCOMMITTED" | "READ COMMITTED" | "REPEATABLE READ" | "SERIALIZABLE"; 
+    const reservationTransaction = await this.connection.manager.transaction(
+      "SERIALIZABLE",
+      async (transactionalEntityManager)=> {
+        const reservation = transactionalEntityManager.create(Reservation, { showDetail, user });
+        // 예매 생성
+        await transactionalEntityManager.save(reservation);
 
-    // 예매 완료시 사용자의 point - price
-    await this.usersRepository.update(
-      { id: userId },
-      { point: user.point - price },
-    );
+        // 예매 완료시 사용자의 point - price
+        await transactionalEntityManager.update(
+          User,
+          { id: userId },
+          { point: user.point - price },
+        );
 
-    // 예매 완료시 showDetail의 reservatedSeat +1
-    await this.showDetailsRepository.update(
-      { id: showDetailId },
-      { reservatedSeat: showDetail.reservatedSeat + 1 },
-    );
-    // ---- 트랜잭션 걸어야 되는 부분 ----
+        // 예매 완료시 showDetail의 reservatedSeat +1
+        await transactionalEntityManager.update(
+          ShowDetail,
+          { id: showDetailId },
+          { reservatedSeat: showDetail.reservatedSeat + 1 },
+        );
+      
+        return reservation;
+    });
 
-    return reservation;
+    // 성형
+    const response = {
+      id: reservationTransaction.id,
+      status: reservationTransaction.status,
+      createdAt: reservationTransaction.createdAt,
+      updatedAt: reservationTransaction.updatedAt,
+      shows: {
+        show_id: showDetail.show.id,
+        show_detail_id: showDetail.id,
+        title: showDetail.show.title,
+        category: showDetail.show.category,
+        location: showDetail.show.location,
+        price: showDetail.show.price,
+        showDate: showDetail.showDate,
+      }
+    };
+  
+    return response;
   }
 
   async getReservations(userId: number) {
     if (_.isNil(userId)) {
       throw new NotFoundException("사용자 아이디가 존재하지 않습니다.");
     }
-    const reservations = await this.reservationsRepository.find({ where: { user: { id: userId } }, relations: ["showDetail"] });
-    return reservations;
+
+    // const reservations = await this.reservationsRepository.find({ where: { user: { id: userId } }, relations: ["showDetail"] });
+
+    // QueryBuilder
+    const reservations = await this.reservationsRepository
+    .createQueryBuilder("reservation")
+    .innerJoinAndSelect("reservation.showDetail", "showDetail")
+    .innerJoinAndSelect("showDetail.show", "show")
+    .where("reservation.user_id = :userId", { userId })
+    .getMany();
+
+    if(_.isEmpty(reservations)) {
+      throw new NotFoundException("예매 내역이 존재하지 않습니다.");
+    }
+    
+    const response = reservations.map((reservation) => ({
+      id: reservation.id,
+      status: reservation.status,
+      createdAt: reservation.createdAt,
+      updatedAt: reservation.updatedAt,
+      shows: {
+        show_id: reservation.showDetail.show.id,
+        show_detail_id: reservation.showDetail.id,
+        title: reservation.showDetail.show.title,
+        category: reservation.showDetail.show.category,
+        location: reservation.showDetail.show.location,
+        price: reservation.showDetail.show.price,
+        showDate: reservation.showDetail.showDate,
+      }
+    }));
+
+    return response;
   }
 }
